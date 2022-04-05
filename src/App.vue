31a7654bb0928d9637cfe1bee91388cb00991284
ChenXander <cx1197257655@gmail.com>
2022-04-05 21:00:15 +0800

    feat: 体验Element Plus

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
+  <div class="app">
+    <h2>{{ $store.state.name }}</h2>
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+    <router-view></router-view>
 
-    <router-link to="/about">About</router-link>
-  </nav>
-
-  <router-view />
+    <el-button type="primary">哈哈哈</el-button>
+    <el-button type="success">Success</el-button>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-  -webkit-font-smoothing: antialiased;
-
-  -moz-osx-font-smoothing: grayscale;
-
-  text-align: center;
-
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return {}
   }
-}
-</style>
+})
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/views/login/LoginIndex.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">登录</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginIndex',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="less" scoped></style>
