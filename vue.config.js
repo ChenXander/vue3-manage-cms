5d667865405694fd27f26cd11ec92a58aa83123b
ChenXander <cx1197257655@gmail.com>
2022-04-06 14:00:31 +0800

    feat: elementPlus

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+// elementPlus自动按需导入
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = defineConfig({
+  outputDir: './build',
+  publicPath: './',
   configureWebpack: {
+    // elementPlus自动按需导入插件
     plugins: [
       AutoImport({
         resolvers: [ElementPlusResolver()]
